423b7b952d0c03ef4135672aaeb3e63e2c426c96
Sarhad <sarhad@dasmeta.com>
2024-12-24 19:41:27 +0400

    fix(DMVP-5181): fix sqs policy

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   name_formed   = replace(var.name, "/\\W|_|\\s/", "-")
   iam_user_name = coalesce(var.iam_user_name, "${local.name_formed}-s3-usr")
+  queue_name    = replace("${var.name}-${var.event_notification_config.name_suffix}", "/\\W+/", "_")
 }
